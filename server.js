1a8bf7cdee5c52436ab2a296348a2e09002f9593
Prakash7078 <20bq1a05j5@vvit.net>
2023-02-22 23:50:04 +0530

    fourth commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
-import userRoutes from './routes/userRoutes.js';
 import msgRoutes from './routes/msgRoutes.js';
 import cors from 'cors';
 import productRouter from './routes/productRoutes.js';
@@ -17,7 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use('/api/seed',seedRouter);
-app.use('/api/users',userRoutes);
 app.use('/api/msgs',msgRoutes);
 app.use('/api/products',productRouter);
 app.use((err,req,res,next)=>{
