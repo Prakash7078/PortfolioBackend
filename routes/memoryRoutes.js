c68705772233f72d951106187dae125195e8b3bf
Prakash7078 <20bq1a05j5@vvit.net>
2023-03-03 23:48:58 +0530

    update memoryRouter2

--- routes/memoryRoutes.js
+++ routes/memoryRoutes.js
@@ -3,7 +3,7 @@ import expressasynchandler from 'express-async-handler';
 import memory from '../models/memoryModel.js';
 const memrouter=express.Router();
 memrouter.get('/comments',async(req,res)=>{
-    const comments=memory.find();
+    const comments=await memory.find();
     res.send(comments);
 })
 memrouter.post('/memory',expressasynchandler(async(req,res)=>{
