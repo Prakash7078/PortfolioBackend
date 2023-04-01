ca1cc123c4ffbe75e8f4430fdb3a80ab6cbf6fd3
Prakash7078 <20bq1a05j5@vvit.net>
2023-04-01 22:34:41 +0530

    uploads folder update

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,30 +1,50 @@
 import multer from 'multer';
 import postModel from '../models/postModel.js';
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
 const postRouter = express.Router();
-const upload = multer({ dest: 'uploads/' });
- // specify the destination folder for uploaded files
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const uploadDir = path.join(__dirname, '../uploads');
+const upload = multer({ dest: uploadDir });
+const app=express();
+// Make sure the uploads folder exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 postRouter.get('/certificates', async (req, res) => {
   const certificates = await postModel.find();
   res.send(certificates);
 });
 
-postRouter.post('/certificate', upload.single('selectedFile'), async (req, res) => {
+postRouter.post('/certificate', upload.single('selectedFile'), async (req, res, next) => {
   const { name, issue, url, skills } = req.body;
   const newCertificate = new postModel({
     name,
     issue,
     url,
-    selectedFile: req.file.filename, // store the filename in the database instead of the file itself
+    selectedFile: req.file.filename,
     skills,
   });
+
   try {
     const certi = await newCertificate.save();
     res.status(201).json(certi);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    next(error);
   }
 });
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: err.message });
+});
+
+console.log('Uploads folder:', uploadDir);
 
 export default postRouter;
