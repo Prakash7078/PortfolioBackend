b0d82bbb0c6934e9fc2189bd3fe10b03d15c81c2
Prakash7078 <20bq1a05j5@vvit.net>
2023-04-01 20:07:34 +0530

    last commit for multer

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import memrouter from './routes/memoryRoutes.js';
 import postRouter from './routes/postRoutes.js';
 
 const app=express();
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
